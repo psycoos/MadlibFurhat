136154b0a78aecf75baf83965eece81d168fb234
Psycoos <coospot4@gmail.com>
2021-03-22 16:56:49 +0100

    beetje aankloten

--- src/main/kotlin/furhatos/app/newskill/flow/interaction.kt
+++ src/main/kotlin/furhatos/app/newskill/flow/interaction.kt
@@ -7,14 +7,26 @@ import furhatos.app.newskill.nlu.*
 val Start : State = state(Interaction) {
 
     onEntry {
-        furhat.ask("Hi there. Do you like robots?")
+        furhat.ask("Hi there. Would you like to create a story together?")
     }
 
     onResponse<Yes>{
-        furhat.say("I like humans.")
+        furhat.say("Alright. Let's begin.")
+        goto(Story)
     }
 
     onResponse<No>{
         furhat.say("That's sad.")
+    }
+}
+
+val Story = state {
+    onEntry {
+        furhat.ask("Please give me an animal")
+    }
+
+    onResponse<Animal> {
+        furhat.say("${it.text}, I will add it to the story.")
+        reentry()
     }
 }
--- src/main/kotlin/furhatos/app/newskill/nlu/nlu.kt
+++ src/main/kotlin/furhatos/app/newskill/nlu/nlu.kt
@@ -1,1 +1,31 @@
 package furhatos.app.newskill.nlu
+
+import furhatos.nlu.*
+import furhatos.nlu.grammar.Grammar
+import furhatos.nlu.kotlin.grammar
+import furhatos.nlu.common.Number
+import furhatos.util.Language
+
+class Animal : Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("dog", "cat", "horse", "ant")
+    }
+}
+//
+//class WhoEntity: WildcardEntity("who", SendMessageIntent())
+//class MessageEntity: WildcardEntity("message", SendMessageIntent())
+//
+//class SendMessageIntent(): Intent() {
+//
+//    var who: WhoEntity? = null
+//    var message: MessageEntity? = null
+//
+//    override fun getExamples(lang: Language): List<String> {
+//        return listOf(
+//                "Tell @who to @message",
+//                "Send @who a message saying @message"
+//        );
+//    }
+//
+//}
+
