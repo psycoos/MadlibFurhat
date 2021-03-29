4481fd2b8f9dccdf34b4011faa369f9a7513793f
QuiH <47984058+QuiH@users.noreply.github.com>
2021-03-29 16:47:45 +0200

    interaction elaborated

--- src/main/kotlin/furhatos/app/newskill/flow/interaction.kt
+++ src/main/kotlin/furhatos/app/newskill/flow/interaction.kt
@@ -4,7 +4,62 @@ import furhatos.nlu.common.*
 import furhatos.flow.kotlin.*
 import furhatos.app.newskill.nlu.*
 
+import furhatos.flow.kotlin.*
+import furhatos.gestures.Gestures
+import furhatos.nlu.common.*
+
+
 val words: MutableList<String> = ArrayList()
+
+
+val Start : State = state(Interaction) {
+
+    onEntry {
+        random(
+            {   furhat.say("Hello") },
+            {   furhat.say("Oh, hello welcome") },
+            {   furhat.say("Hi") }
+        )
+        furhat.say("I am Furhat.")
+        furhat.gesture(Gestures.BigSmile(duration=2.0))
+        delay(300)
+
+        goto(AskToPlay)
+
+
+    }
+}
+val AskToPlay = state(Interaction) {
+    onEntry {
+        random(
+            {furhat.ask("Would you like to make a Mad lib with me")},
+            {furhat.ask("Do you want to make a Mad lib?")}
+        )
+
+    }
+
+    onResponse<Yes>{
+        random(
+            {furhat.say("Great, let's start!")},
+            {furhat.say("Awesome, let's go!")}
+        )
+        furhat.gesture(Gestures.Smile, async = false)
+
+
+        furhat.say("A Madlib works as follows. I will ask you to come up with certain types of words. " +
+                "Then I will use them to form a story.")
+    }
+
+    onResponse<No>{
+        random(
+            {furhat.say("Okay, well that's a shame. Have a nice day!")},
+            {furhat.say("Oh, that's unfortunate. Have a nice day!")}
+        )
+        furhat.gesture(Gestures.ExpressSad, false)
+    }
+
+}
+
 
 val Start : State = state(Interaction) {
 
